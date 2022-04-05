27b8a6fdb5e3e4e8be7ca57115a136de47d7f71c
Jagroop <jagroopbagri5@gmail.com>
2022-04-05 16:08:56 -0700

    Created shopping cart page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Women from './pages/Women';
 import Men from './pages/Men';
 import About from './pages/About';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ShoppingCart from './pages/ShoppingCart';
 function App() {
   return (
     <BrowserRouter>
@@ -12,6 +13,10 @@ function App() {
           <Route path="/women" element={<Women></Women>}></Route>
           <Route path="/men" element={<Men></Men>}></Route>
           <Route path="/about" element={<About></About>}></Route>
+          <Route
+            path="/shopping-cart"
+            element={<ShoppingCart></ShoppingCart>}
+          ></Route>
         </Routes>
       </div>
     </BrowserRouter>
---
+++ src/pages/ShoppingCart.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header';
+function ShoppingCart() {
+  return (
+    <div className="cart--page">
+      <Header></Header>
+      <div className="cart--title--container">
+        <p>Shopping Cart</p>
+      </div>
+    </div>
+  );
+}
+
+export default ShoppingCart;
